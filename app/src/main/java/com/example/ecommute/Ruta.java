70e4aee7428dc28858266b7e5f2cdfa53d1faa9e
Joan <joanmarinellobernadet@gmail.com>
2021-10-20 17:27:17 +0200

    Separació HUs

--- app/src/main/java/com/example/ecommute/Ruta.java
+++ app/src/main/java/com/example/ecommute/Ruta.java
@@ -3,11 +3,13 @@ package com.example.ecommute;
 public class Ruta {
     private String origen;
     private String destino;
+    private int consumo;
+    private int dist = 990; //Se quita y se cambia por llamada a API
 
     public Ruta(String origen, String destino){
         this.origen = origen;
         this.destino = destino;
-        //Posibilidad de guardar la hora / distancia (el segundo necesita API o algo por el estilo)
+        this.consumo = dist * 143; //(143 g/km coche de gasolina medio)
     }
 
     public String getOrigen(){
@@ -16,5 +18,22 @@ public class Ruta {
 
     public String getDestino(){
         return this.destino;
+    }
+
+    public int getConsumo(){
+        return this.consumo;
+    }
+
+    public void setOrigen(String or){
+        this.origen = or;
+    }
+
+    public void setDestino(String dest){
+        this.destino = dest;
+    }
+
+    public void updateRuta(String origen, String destino){
+        this.origen = origen;
+        this.destino = destino;
     }
 }
