b65dfe5f0ad1659c8dba95166ce8b1547d61f021
Joan <joanmarinellobernadet@gmail.com>
2021-10-20 17:41:56 +0200

    Separació HUs

--- app/src/main/java/com/example/ecommute/Ruta.java
+++ app/src/main/java/com/example/ecommute/Ruta.java
@@ -3,11 +3,15 @@ package com.example.ecommute;
 public class Ruta {
     private String origen;
     private String destino;
+    private int consumo;
+    private int dist = 990; //Se quita y se cambia por llamada a API
+    private String comparacion;
 
     public Ruta(String origen, String destino){
         this.origen = origen;
         this.destino = destino;
-        //Posibilidad de guardar la hora / distancia (el segundo necesita API o algo por el estilo)
+        this.consumo = dist * 143; //(143 g/km coche de gasolina medio)
+        this.comparacion = getComparacio(consumo);
     }
 
     public String getOrigen(){
@@ -16,5 +20,21 @@ public class Ruta {
 
     public String getDestino(){
         return this.destino;
+    }
+
+    public int getConsumo(){
+        return this.consumo;
+    }
+
+    public String getComparacion(){
+        return this.comparacion;
+    }
+
+    public String getComparacio(int cons){
+        String comp = "La cantidad de CO2 expulsada en este viaje equivale a: "
+                + cons/200 + " botellas de plastico de 1.5L, o a "
+                + cons/10 + " bolsas de plastico, y ademas se necesitarian "
+                + cons/110 + " dias para que un arbol absorbiera esa cantidad de CO2";
+        return comp;
     }
 }
