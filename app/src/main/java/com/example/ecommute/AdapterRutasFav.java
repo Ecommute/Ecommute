60a0f625fd8d2a7ef1c99fce4bf1bf1a9fe8c7b3
clara0201 <clara.estruch@estudiantat.upc.edu>
2021-11-02 13:41:04 +0100

    Añadido botón de favorito (funcionalidades añadir y quitar)

--- app/src/main/java/com/example/ecommute/AdapterRutasFav.java
+++ app/src/main/java/com/example/ecommute/AdapterRutasFav.java
@@ -1,13 +1,18 @@
 package com.example.ecommute;
 
+import static com.example.ecommute.R.drawable.*;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ecommute.R.drawable;
 
 public class AdapterRutasFav extends RecyclerView.Adapter<AdapterRutasFav.ViewHolderRF> {
     Context mContext;
@@ -45,20 +50,29 @@ public class AdapterRutasFav extends RecyclerView.Adapter<AdapterRutasFav.ViewHo
     public class ViewHolderRF extends RecyclerView.ViewHolder {
         TextView mTextOrigen, mTextDestino;
         Integer id;
+        boolean fav = false;
 
         public ViewHolderRF(@NonNull View itemView) {
             super(itemView);
             mTextOrigen = itemView.findViewById(R.id.origen2);
             mTextDestino = itemView.findViewById(R.id.destino2);
 
-            /*Button iButton = itemView.findViewById(R.id.item_button2);
-            iButton.setOnClickListener(new View.OnClickListener() {
+            Button starB = itemView.findViewById(R.id.starButton);
+            starB.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View vp) {
-                    //Afegir-ho al buscar ruta somehow
+                    if(!fav){
+                        starB.setBackgroundResource(ic_baseline_star_24);
+                        //add ruta to favorites
+                    }
+                    else{
+                        starB.setBackgroundResource(ic_baseline_star_border_24);
+                        //remove from favorites
+                    }
+
                 }
-            });*/
+            });
         }
 
         public void setId(Integer id) {
