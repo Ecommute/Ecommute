7cb3780f6c5ea8d1c2e7a235c285ee16008f7173
clara0201 <clara.estruch@estudiantat.upc.edu>
2021-11-14 17:09:46 +0100

    Pruebas

--- app/src/main/java/com/example/ecommute/AdapterHistorial.java
+++ app/src/main/java/com/example/ecommute/AdapterHistorial.java
@@ -1,10 +1,10 @@
 package com.example.ecommute;
 
-import static com.example.ecommute.R.drawable.ic_baseline_error_24;
-import static com.example.ecommute.R.drawable.ic_baseline_star_24;
-import static com.example.ecommute.R.drawable.ic_baseline_star_border_24;
+import static android.content.ContentValues.TAG;
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +14,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-import com.example.ecommute.databinding.ItemHistorialBinding;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -29,7 +31,7 @@ public class AdapterHistorial extends RecyclerView.Adapter<AdapterHistorial.MyVi
     Context mContext;
     String[] mStrOrigenes, mStrDestinos;
     Integer[] mStrPuntos, mIntIds, mIntFavs;
-    private ItemHistorialBinding bindingI;
+
 
     public AdapterHistorial(Context context, String[] strOrigenes, String[] strDestinos, Integer[] strPuntos, Integer[] intIds, Integer[] intFavs){
         mContext = context;
@@ -54,9 +56,11 @@ public class AdapterHistorial extends RecyclerView.Adapter<AdapterHistorial.MyVi
         holder.mTextOrigen.setText(mStrOrigenes[position]);
         holder.mTextDestino.setText(mStrDestinos[position]);
         holder.mTextPuntos.setText(new StringBuilder().append(mStrPuntos[position]).append(" kg de CO2").toString());
-        if(mIntFavs[position] == null) holder.fav = -1;
-        else holder.fav = mIntFavs[position];
+        holder.fav = mIntFavs[position];
         holder.id = mIntIds[position];
+
+        Log.d("RID BINDING", String.valueOf(mIntIds[position]));
+        Log.d("RID BINDING F", String.valueOf(mIntFavs[position]));
     }
 
     @Override
@@ -68,7 +72,9 @@ public class AdapterHistorial extends RecyclerView.Adapter<AdapterHistorial.MyVi
         public String username = GlobalVariables.username;
         public String password = GlobalVariables.password;
         public TextView mTextOrigen, mTextDestino, mTextPuntos;
-        public Integer id, fav;
+        public Integer id;
+        public Integer fav = -1;
+        //public boolean fav;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -76,10 +82,44 @@ public class AdapterHistorial extends RecyclerView.Adapter<AdapterHistorial.MyVi
             mTextDestino = itemView.findViewById(R.id.destino);
             mTextPuntos = itemView.findViewById(R.id.puntos);
 
+            setUp();
+
+            /*final Response[] response = new Response[1];
+
+            OkHttpClient client = new OkHttpClient().newBuilder().build();
+            Request request = new Request.Builder()
+                    .url("http://10.4.41.35:3000/routes/show/" + id + "?username=" + username + "&password=" + password)
+                    .method("GET", null)
+                    .build();
+            try {
+                response[0] = client.newCall(request).execute();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            Log.d("RID:", String.valueOf(id));
+            String jsonData;
+
+            fav = -1;
+            try {
+                jsonData = Objects.requireNonNull(response[0].body()).string();
+                JSONObject jo = new JSONObject((jsonData));
+                JSONObject routes = jo.getJSONObject("route");
+
+                fav = Integer.parseInt(routes.getString("favourite"));
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }*/
+        }
+
+        private void setUp() {
+            Log.d("RID FAV", String.valueOf(fav));
+
             Button starB = itemView.findViewById(R.id.starButton);
-            if(fav == null) starB.setBackgroundResource(ic_baseline_error_24);
-            else if(fav == 0) starB.setBackgroundResource(ic_baseline_star_24);
-            else starB.setBackgroundResource(ic_baseline_star_border_24);
+            if(fav == -1) starB.setBackgroundResource(R.drawable.ic_baseline_error_24);
+            if(fav == 1) starB.setBackgroundResource(R.drawable.ic_baseline_star_24);
+            else starB.setBackgroundResource(R.drawable.ic_baseline_star_border_24);
 
             Button iButton = itemView.findViewById(R.id.item_button);
             iButton.setOnClickListener(new View.OnClickListener() {
@@ -91,15 +131,19 @@ public class AdapterHistorial extends RecyclerView.Adapter<AdapterHistorial.MyVi
                 }
             });
 
+            Log.d("RID CLICK BF", String.valueOf(id));
+
 
             starB.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View vp) {
-                    if(fav == 0){
-                        fav = 1;
-                        starB.setBackgroundResource(ic_baseline_star_24);
 
+                    if(fav == 1){
+                        fav = 0;
+                        starB.setBackgroundResource(R.drawable.ic_baseline_star_24);
+
+                        Log.d("RID CLICK", String.valueOf(id));
                         //add ruta to favorites
                         final Response[] response = new Response[1];
 
@@ -119,8 +163,8 @@ public class AdapterHistorial extends RecyclerView.Adapter<AdapterHistorial.MyVi
 
                     }
                     else{
-                        fav = 0;
-                        starB.setBackgroundResource(ic_baseline_star_border_24);
+                        fav = 1;
+                        starB.setBackgroundResource(R.drawable.ic_baseline_star_border_24);
 
                         //remove from favorites
                         final Response[] response2 = new Response[1];
@@ -139,11 +183,8 @@ public class AdapterHistorial extends RecyclerView.Adapter<AdapterHistorial.MyVi
                             e.printStackTrace();
                         }
                     }
-
                 }
             });
-
-
 
         }
     }
