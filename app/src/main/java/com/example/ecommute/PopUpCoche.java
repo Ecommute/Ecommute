4d2373295dea77ab0272e96520f9862bcd4058fe
Joan <joanmarinellobernadet@gmail.com>
2021-11-17 09:26:43 +0100

    Afegir coche funciona!

--- app/src/main/java/com/example/ecommute/PopUpCoche.java
+++ app/src/main/java/com/example/ecommute/PopUpCoche.java
@@ -1,6 +1,7 @@
 package com.example.ecommute;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.DisplayMetrics;
@@ -10,10 +11,20 @@ import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
-import com.example.ecommute.databinding.PopUpCocheBinding;
+import com.example.ecommute.ui.perfil.PerfilFragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class PopUpCoche extends Activity {
-    private PopUpCocheBinding binding;
 
 
 
@@ -22,8 +33,6 @@ public class PopUpCoche extends Activity {
         super.onCreate(savedInstanceState);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        binding = PopUpCocheBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
         setContentView(R.layout.pop_up_coche);
 
@@ -35,16 +44,62 @@ public class PopUpCoche extends Activity {
 
         getWindow().setLayout((int)(width*0.6), (int)(height*0.6));
 
+        String fuel = null;
+        String consumption = null;
+        final Response[] response = new Response[1];
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://10.4.41.35:3000/users/cardetails?username=" + GlobalVariables.username + "&password=" + GlobalVariables.password)
+                .method("GET", null)
+                .build();
+        try {
+            response[0] = client.newCall(request).execute();
+            JSONObject respuesta = new JSONObject(response[0].body().string());
+            fuel =  respuesta.getString("fuelType");
+            consumption = respuesta.getString("fuelConsumption");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(fuel != null && consumption != null){
+            EditText editFuel = findViewById(R.id.fuelType);
+            EditText editConsumo = findViewById(R.id.consumo);
+            editFuel.setText(fuel);
+            editConsumo.setText(consumption);
+        }
+
+
         Button guardar = findViewById(R.id.guardar_coche);
         guardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editCoche = binding.nombreCoche;
-                EditText editConsumo = binding.consumo;
-
+                EditText editFuel = findViewById(R.id.fuelType);
+                EditText editConsumo = findViewById(R.id.consumo);
                 //API
 
+                final Response[] response2 = new Response[1];
+                OkHttpClient client2 = new OkHttpClient().newBuilder()
+                        .build();
+                MediaType mediaType = MediaType.parse("text/plain");
+                RequestBody body = RequestBody.create("", mediaType);
+                Request request2 = new Request.Builder()
+                        .url("http://10.4.41.35:3000/users/cardetails?username="+ GlobalVariables.username +"&password="+ GlobalVariables.password +"&fuelType="+ editFuel.getText().toString() +"&fuelConsumption="+ editConsumo.getText().toString())
+                        .method("PUT", body)
+                        .build();
 
+                try {
+                    response2[0] = client2.newCall(request2).execute();
+                    JSONObject respuesta2 = new JSONObject(response2[0].body().string());
+                    JSONObject Jarray = new JSONObject(respuesta2.getString("result"));
+
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Intent intent = new Intent(PopUpCoche.this, PerfilFragment.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
             }
         });
 
