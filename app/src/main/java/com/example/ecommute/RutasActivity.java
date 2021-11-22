27fd4ffc6b0f05b28e21a7509dd10b3f9a878858
carlosraschev <carlos.rasche@estudiantat.upc.edu>
2021-11-22 16:45:17 +0100

    millores demo sprint 2

--- app/src/main/java/com/example/ecommute/RutasActivity.java
+++ app/src/main/java/com/example/ecommute/RutasActivity.java
@@ -20,13 +20,22 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class RutasActivity extends AppCompatActivity {
 
     private ActivityRutasBinding binding;
+    private String iframe;
+    private Button walking;
+    private Button driving;
+    private Button bicycling;
+    private Button transit;
+    private Button guardar;
+    private String method;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +45,120 @@ public class RutasActivity extends AppCompatActivity {
         binding = ActivityRutasBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        String iframe = "<iframe\n        width=\"450\"\n        height=\"250\"\n        frameborder=\"0\" style=\"border:0\"\n        src=https://www.google.com/maps/embed/v1/directions?key=AIzaSyApUk0xJoZuc46YAjVQEhF1ul67ObY80Sk&origin=Barcelona&destination=Madrid&mode=driving&region=es allowfullscreen>\n    </iframe>";
+        //Get map iframe
+        try {
+            getMap();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
         WebView googleMapWebView = binding.googlemapWebView;
         googleMapWebView.getSettings().setJavaScriptEnabled(true);
         googleMapWebView.loadData(iframe, "text/html", "utf-8");
 
+        walking = binding.walking;
+        driving = binding.driving;
+        bicycling = binding.bicycling;
+        transit = binding.transit;
+        guardar = binding.guardar;
+
+        walking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickWalking();
+            }
+        });
+
+        driving.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickDriving();
+            }
+        });
+
+        bicycling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickBicycling();
+            }
+        });
+
+        transit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickTransit();
+            }
+        });
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    clickGuardar();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void getMap() throws IOException, JSONException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://10.4.41.35:3000/routes/map?origin="+GlobalVariables.origen+"&destination="+GlobalVariables.destino+"&mode=driving&username="+GlobalVariables.username+"&password="+GlobalVariables.password)
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
+        JSONObject respuesta = new JSONObject(response.body().string());
+        iframe = respuesta.getString("iframe");
+    }
+
+    private void clickWalking(){
+        method = "walking";
+        walking.setBackgroundColor(getResources().getColor(R.color.medium_green));
+        driving.setBackgroundColor(getResources().getColor(R.color.light_green));
+        bicycling.setBackgroundColor(getResources().getColor(R.color.light_green));
+        transit.setBackgroundColor(getResources().getColor(R.color.light_green));
+
+    }
+
+    private void clickDriving(){
+        method = "driving";
+        walking.setBackgroundColor(getResources().getColor(R.color.light_green));
+        driving.setBackgroundColor(getResources().getColor(R.color.medium_green));
+        bicycling.setBackgroundColor(getResources().getColor(R.color.light_green));
+        transit.setBackgroundColor(getResources().getColor(R.color.light_green));
+    }
+
+    private void clickBicycling(){
+        method = "bicycling";
+        walking.setBackgroundColor(getResources().getColor(R.color.light_green));
+        driving.setBackgroundColor(getResources().getColor(R.color.light_green));
+        bicycling.setBackgroundColor(getResources().getColor(R.color.medium_green));
+        transit.setBackgroundColor(getResources().getColor(R.color.light_green));
+    }
+
+    private void clickTransit(){
+        method = "transit";
+        walking.setBackgroundColor(getResources().getColor(R.color.light_green));
+        driving.setBackgroundColor(getResources().getColor(R.color.light_green));
+        bicycling.setBackgroundColor(getResources().getColor(R.color.light_green));
+        transit.setBackgroundColor(getResources().getColor(R.color.medium_green));
+    }
+
+    private void clickGuardar() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create("", mediaType);
+        Request request = new Request.Builder()
+                .url("http://10.4.41.35:3000/routes/add?origin="+GlobalVariables.origen+"&destination="+GlobalVariables.destino+"&mode="+method+"&username="+GlobalVariables.username+"&password="+GlobalVariables.password)
+                .method("POST", body)
+                .build();
+        Response response = client.newCall(request).execute();
+        Intent intent = new Intent(RutasActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
