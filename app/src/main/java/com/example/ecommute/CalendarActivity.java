a162453b6c7dd3c0e5b7ba88ed75aeeda02da3f3
teyteymey <tatiana.meyer.u@gmail.com>
2021-12-11 16:56:08 +0100

    Creación de la view calendario

---
+++ app/src/main/java/com/example/ecommute/CalendarActivity.java
@@ -0,0 +1,30 @@
+package com.example.ecommute;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity extends Activity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        Log.d("botonPulsado", "oncreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
+    }
+}
