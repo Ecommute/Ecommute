c7dfe1edb21b25fe516ab6319d71628994cfe2d6
Joan <joanmarinellobernadet@gmail.com>
2022-01-12 00:21:27 +0100

    Parche eventos no logged google

--- app/src/main/java/com/example/ecommute/PopUpEventos.java
+++ app/src/main/java/com/example/ecommute/PopUpEventos.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,9 @@ import okhttp3.Response;
 
 public class PopUpEventos extends AppCompatActivity {
 
+
+    String[] arrayTitulos;
+    String[] arrayLocalitzacions;
     RecyclerView ListaEventos;
     RecyclerView.LayoutManager mLayoutManager;
 
@@ -54,36 +58,40 @@ public class PopUpEventos extends AppCompatActivity {
 
     private void setUpRecycler() throws IOException, JSONException {
 
-        Log.d("Auth", GlobalVariables.authcode);
+        if(GlobalVariables.authcode != null){
 
-        final Response[] response = new Response[1];
+            final Response[] response = new Response[1];
 
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
+            OkHttpClient client = new OkHttpClient().newBuilder().build();
 
-        Request request = new Request.Builder()
-                .url("http://10.4.41.35:3000/calendar/getEvent?time=" + GlobalVariables.fecha + "&code=" + GlobalVariables.authcode
-                        + "&username=" + GlobalVariables.username + "&password=" + GlobalVariables.password)
-                .method("GET", null)
-                .build();
+            Request request = new Request.Builder()
+                    .url("http://10.4.41.35:3000/calendar/getEvent?time=" + GlobalVariables.fecha + "&code=" + GlobalVariables.authcode
+                            + "&username=" + GlobalVariables.username + "&password=" + GlobalVariables.password)
+                    .method("GET", null)
+                    .build();
 
-        response[0] = client.newCall(request).execute();
+            response[0] = client.newCall(request).execute();
 
-        String jsonData = response[0].body().string();
-        JSONObject Jobject = new JSONObject(jsonData);
-        JSONArray Jarray = Jobject.getJSONArray("events");
+            String jsonData = response[0].body().string();
+            JSONObject Jobject = new JSONObject(jsonData);
+            JSONArray Jarray = Jobject.getJSONArray("events");
 
-        int n = Jarray.length();
-        String[] arrayTitulos = new String[n];
-        String[] arrayLocalitzacions = new String[n];
+            int n = Jarray.length();
+            String[] arrayTitulos = new String[n];
+            String[] arrayLocalitzacions = new String[n];
 
-        for (int i = 0; i < n; i++) {
-            JSONObject object = Jarray.getJSONObject(i);
-            if (object.has("summary")) {
-                arrayTitulos[i] = object.getString("summary");
-            } else arrayTitulos[i] = "No title";
-            if (object.has("location")) {
-                arrayLocalitzacions[i] = object.getString("location");
-            } else arrayLocalitzacions[i] = "No location";
+            for (int i = 0; i < n; i++) {
+                JSONObject object = Jarray.getJSONObject(i);
+                if (object.has("summary")) {
+                    arrayTitulos[i] = object.getString("summary");
+                } else arrayTitulos[i] = "No title";
+                if (object.has("location")) {
+                    arrayLocalitzacions[i] = object.getString("location");
+                } else arrayLocalitzacions[i] = "No location";
+            }
+
+        } else {
+            Toast.makeText(this, "Debes logearte con Google para ver tus eventos!", Toast.LENGTH_LONG).show();
         }
 
         ListaEventos = findViewById(R.id.EventosView);
