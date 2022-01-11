cdb969a1fc0c7376382579b56deb2c0de5f2a9e2
clara0201 <clara.estruch@estudiantat.upc.edu>
2022-01-11 17:18:42 +0200

    Mejoras Report y Delete comentario

--- app/src/main/java/com/example/ecommute/AdapterComment.java
+++ app/src/main/java/com/example/ecommute/AdapterComment.java
@@ -1,14 +1,20 @@
 package com.example.ecommute;
 
+import static android.text.format.DateUtils.getRelativeTimeSpanString;
+
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
@@ -18,6 +24,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+
 
 public class AdapterComment extends RecyclerView.Adapter<AdapterComment.ViewHolder> {
 
@@ -52,14 +59,21 @@ public class AdapterComment extends RecyclerView.Adapter<AdapterComment.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if(mOwns[position]) {
+            holder.itemView.setVisibility(View.GONE);
+        }
+        else {
+            holder.author.setText(mAuthors[position]);
+            holder.content.setText(mContents[position]);
+            holder.createdAt = mCreatedAts[position];
+            holder.id = mIds[position];
+            holder.idArticle = midArticle;
+            holder.own = mOwns[position];
+            holder.reported = mReporteds[position];
 
-        holder.author.setText(mAuthors[position]);
-        holder.content.setText(mContents[position]);
-        holder.createdAt.setText(mCreatedAts[position]);
-        holder.id = mIds[position];
-        holder.idArticle = midArticle;
-        holder.own = mOwns[position];
-        holder.reported = mReporteds[position];
+            if(mOwns[position]) holder.report.setVisibility(View.GONE);
+            else holder.delete.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -71,28 +85,34 @@ public class AdapterComment extends RecyclerView.Adapter<AdapterComment.ViewHold
         public String username = GlobalVariables.username;
         public String password = GlobalVariables.password;
 
-        public TextView author, content, createdAt;
+        public TextView author, content, createdAtPretty;
+        public String createdAt;
         public Integer id, idArticle;
         public Boolean own, reported;
+        public ImageButton report, delete;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             author = itemView.findViewById(R.id.commAuthor);
             content = itemView.findViewById(R.id.commContent);
-            createdAt = itemView.findViewById(R.id.commCreatedAt);
+            createdAtPretty = itemView.findViewById(R.id.commCreatedAt);
 
-            /*Button report = itemView.findViewById(R.id.report);
+            report = itemView.findViewById(R.id.report);
+            delete = itemView.findViewById(R.id.delete);
+
             report.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    report.setImageResource(R.drawable.ic_baseline_error_red);
+
                     final Response[] response2 = new Response[1];
 
                     OkHttpClient client = new OkHttpClient().newBuilder().build();
                     MediaType mediaType = MediaType.parse("text/plain");
                     RequestBody body = RequestBody.create("", mediaType);
                     Request request = new Request.Builder()
-                            .url("10.4.41.35:3000/articles/" + idArticle + "/comment/" + id + "/report?username=" + username + "&password=" + password)
+                            .url("http://10.4.41.35:3000/articles/" + idArticle + "/comment/" + id + "/report?username=" + username + "&password=" + password)
                             .method("POST", body)
                             .build();
                     try {
@@ -100,11 +120,10 @@ public class AdapterComment extends RecyclerView.Adapter<AdapterComment.ViewHold
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
             });
 
-            Button delete = itemView.findViewById(R.id.delete);
+
             delete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -115,7 +134,7 @@ public class AdapterComment extends RecyclerView.Adapter<AdapterComment.ViewHold
                     MediaType mediaType = MediaType.parse("text/plain");
                     RequestBody body = RequestBody.create("", mediaType);
                     Request request = new Request.Builder()
-                            .url("10.4.41.35:3000/articles/" + idArticle + "/comment/" + id + "?username=" + username + "&password=" + password)
+                            .url("http://10.4.41.35:3000/articles/" + idArticle + "/comment/" + id + "?username=" + username + "&password=" + password)
                             .method("DELETE", body)
                             .build();
                     try {
@@ -125,7 +144,7 @@ public class AdapterComment extends RecyclerView.Adapter<AdapterComment.ViewHold
                     }
 
                 }
-            });*/
+            });
         }
     }
 }
